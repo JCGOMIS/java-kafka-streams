4a22f5328591fbcd8b38f3fc716d1ee70609789e
Jeangomistech <jeangomistech2018@gmail.com>
2026-01-20 11:23:41 +0100

    setup input and output-topic

---
+++ k-stream/src/main/java/com/jcgomis/kstream/KStreamApplication.java
@@ -0,0 +1,13 @@
+package com.jcgomis.kstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KStreamApplication.class, args);
+    }
+
+}
