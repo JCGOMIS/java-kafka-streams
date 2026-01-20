05d5a432155ba2b46c5dbd8164d036125412c05b
Jeangomistech <jeangomistech2018@gmail.com>
2026-01-20 12:20:01 +0100

    filtrage de données avec la methode filter

---
+++ k-stream/src/main/java/com/jcgomis/kstream/filtrer/Filter.java
@@ -0,0 +1,40 @@
+package com.jcgomis.kstream.filtrer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Properties;
+import java.util.UUID;
+
+public class Filter {
+
+    private static final String INPUT_TOPIC = "string-input";
+    private static final String OUTPUT_TOPIC = "string-output";
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, UUID.randomUUID().toString());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        final StreamsBuilder builder = new StreamsBuilder();
+         KStream<String, String> source = builder.stream(INPUT_TOPIC);
+
+        source = source.filter((k, v)->{return v.length() > 5 ? true : false;});
+
+        source.to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
+
+        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(),props);
+
+        kafkaStreams.start();
+    }
+}
